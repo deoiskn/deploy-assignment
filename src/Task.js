2fe1124a8cbb8918b1266be19fcc473008084b35
Deo Andrianto Iskandar <111202012699@mhs.dinus.ac.id>
2022-12-16 13:38:50 +0700

    add all file react

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Deo Andrianto Iskandar"; // TODO: replace this
+const studentId = "FE2699434"; // TODO: replace this
+
+export { url, studentName, studentId };
